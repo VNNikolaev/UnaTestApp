0ffddbf7fafa7942621f51c49ee6cbe77c38903b
Nikolaev Vasiliy <vn.nikolaev@vtb.ru>
2022-08-18 12:03:31 +0500

    Initial Commit

---
+++ UnaCashTest/UnaCashTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnaCashTestApp.swift
+//  UnaCashTest
+//
+//  Created by Васлий Николаев on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct UnaCashTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
